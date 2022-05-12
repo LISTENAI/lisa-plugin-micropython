e9cc6c6fcb87f5ea72d0747c25ab826976f2ac03
seasonyuu <seasonyuu@gmail.com>
2022-05-12 18:26:24 +0800

    use-sdk: support use existing path

--- src/tasks/environment.ts
+++ src/tasks/environment.ts
@@ -1,8 +1,9 @@
 import { execFileSync } from 'child_process';
 import { existsSync, rmSync } from 'fs-extra';
-import { join } from 'path';
+import { join, resolve } from 'path';
 import simpleGit, { SimpleGitProgressEvent } from 'simple-git';
 import { GitExecutorResult } from 'simple-git/dist/src/lib/types';
+import { invalidateEnv } from '../env';
 import { PLUGIN_HOME, set } from '../env/config';
 import { job, LisaType } from '../utils/lisa_ex';
 import parseArgs from '../utils/parseArgs';
@@ -30,7 +31,7 @@ export default ({ application, cmd }: LisaType) => {
       const defaultPath = join(PLUGIN_HOME, 'sdk');
 
       const path = args['path'] || defaultPath;
-      const target = path;
+      const target = resolve(path);
 
       let gitRepo = defaultGitRepo;
       if (args['from-git']) {
@@ -39,17 +40,6 @@ export default ({ application, cmd }: LisaType) => {
 
       if (target && /.*[\u4e00-\u9fa5]+.*$/.test(target)) {
         throw new Error(`SDK 路径不能包含中文: ${target}`);
-      }
-
-      if (existsSync(target) && !args.update) {
-        // if is windows
-        if (process.platform === 'win32') {
-          await cmd('del', [target, '/s', '/q', '/f', '/a'], {
-            stderr: 'inherit',
-          });
-        } else {
-          rmSync(target, { recursive: true, force: true });
-        }
       }
 
       if (!args.update) {
@@ -72,13 +62,44 @@ export default ({ application, cmd }: LisaType) => {
         };
         const gitOptions = { progress, errors };
 
-        console.log('拉取最新 SDK ...');
-        await simpleGit(gitOptions).clone(gitRepo, path, ['--depth=1']);
+        if (existsSync(path)) {
+          console.log('SDK 路径不为空，尝试更新 SDK...');
+
+          try {
+            await cmd('git', ['pull'], {
+              stdio: 'inherit',
+              cwd: path,
+            });
+          } catch (e) {
+            console.log('更新失败，尝试删除文件夹...');
+
+            if (process.platform === 'win32') {
+              await cmd('del', [target, '/s', '/q', '/f', '/a'], {
+                stderr: 'inherit',
+              });
+            } else {
+              rmSync(target, { recursive: true, force: true });
+            }
+
+            console.log('拉取最新 SDK ...');
+            await simpleGit(gitOptions).clone(gitRepo, path, ['--depth=1']);
+          }
+        } else {
+          console.log('拉取最新 SDK ...');
+          await simpleGit(gitOptions).clone(gitRepo, path, ['--depth=1']);
+        }
 
         console.log('更新子模块 ...');
         await cmd(
           'git',
-          ['submodule', 'update', '--force', '--init', '--recursive', '--depth=1'],
+          [
+            'submodule',
+            'update',
+            '--force',
+            '--init',
+            '--recursive',
+            '--depth=1',
+          ],
           {
             cwd: target,
             stdio: 'inherit',
@@ -97,6 +118,8 @@ export default ({ application, cmd }: LisaType) => {
           stdio: [process.stdin, process.stdout, process.stderr],
         });
       }
+
+      invalidateEnv();
 
       await set('sdk', target);
       console.log(args.update ? '更新 SDK 成功' : '设置 SDK 成功');
