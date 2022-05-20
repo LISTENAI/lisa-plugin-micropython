0fad50351456013bb51bde92d40c7f142b3b09b0
Assassinss <zhu948742@gmail.com>
2022-05-20 12:12:01 +0800

    Supported install package dependencies

--- src/tasks/pip.ts
+++ src/tasks/pip.ts
@@ -9,9 +9,8 @@ import unzip from '../utils/unzip';
 const PACKAGE_URL = 'https://micropython.org/pi';
 const PACKAGE_PATH = 'py/_slash_lib';
 
-export default ({ application, cmd, runner }: LisaType) => {
+export default ({ application }: LisaType) => {
   job('pip:install', {
-    title: '安装Micropython库',
     async task(ctx, task) {
       const prjPath = join(process.cwd(), 'prj.conf');
       const mainPath = join(process.cwd(), 'py/main.py');
@@ -25,13 +24,16 @@ export default ({ application, cmd, runner }: LisaType) => {
       }
       const name = argv[argv.length - 1];
 
+      task.title = `安装${name}`;
+
       const getPackage = async (name: string): Promise<{ version: string, url: string }> => {
         const reqUrl = `${PACKAGE_URL}/${name}/json`;
         const data = await (await got.get(reqUrl)).body;
         const result = JSON.parse(data);
+        const urls = result.releases[result.info.version]
         return {
           version: result.info.version,
-          url: result.releases[result.info.version][0].url
+          url: urls[urls.length - 1].url
         };
       };
 
@@ -40,17 +42,15 @@ export default ({ application, cmd, runner }: LisaType) => {
        */
       const packageInfo = await getPackage(name);
 
-      const createDirIfNotExist = async (dir: string) => {
-        const path = join(process.cwd(), dir);
+      const createDirIfNotExist = async (dir: string): Promise<void> => {
         const pathList = dir.split('/').filter(item => item.length > 0);
         let currentPath = process.cwd();
-        pathList.forEach(async item => {
+        for (const item of pathList) {
           currentPath = join(currentPath, item);
           if (!existsSync(currentPath)) {
             await mkdir(currentPath);
           }
-        });
-        return path;
+        }
       };
 
       /** 
@@ -58,27 +58,69 @@ export default ({ application, cmd, runner }: LisaType) => {
        */
       await createDirIfNotExist(PACKAGE_PATH);
 
+      const download = async (url: string): Promise<Buffer> => {
+        return await (await got.get(url)).rawBody;
+      };
+
       /**
        * 3. download package
        */
-      const data = await (await got.get(packageInfo.url)).rawBody;
+      const data = await download(packageInfo.url);
+
+      const installed: string[] = [];
+      installed.push(name);
+
+      /**
+       * download and save dependencies package
+       * @param deps 
+       */
+      const installDeps = async (deps: string[]): Promise<void> => {
+        for (const dep of deps) {
+          if (!installed.includes(dep)) {
+            const pkgInfo = await getPackage(dep);
+
+            const data = await download(pkgInfo.url);
+            await install(data);
+
+            installed.push(dep);
+          }
+        }
+      };
+
+      const install = async (data: Buffer): Promise<void> => {
+        const zip = await unzip(data, 'PKG-INFO');
+        for (const [path, buffer] of Object.entries(zip)) {
+          const items = path.split('/').filter(item => item.length > 0);
+          const pyPath = path.replaceAll(`${items[0]}/`, '');
+
+          const isRequires = path.match(/requires.txt/);
+
+          if (!isRequires) {
+            const subPath = path.match('/.+/');
+            if (subPath) {
+              const libPath = join(PACKAGE_PATH, subPath[0]);
+              await createDirIfNotExist(libPath);
+            }
+
+            const file = join(PACKAGE_PATH, pyPath);
+            await writeFile(file, buffer);
+          }
+
+          /**
+           * install dependencies
+           */
+          if (path.match(/requires.txt/)) {
+            //get dependencies
+            const deps = buffer.toString().split('\n').filter(item => item.length > 0);
+            await installDeps(deps);
+          }
+        }
+      };
 
       /**
        * 4. unzip and save package to package directory
        */
-      const zip = await unzip(data, 'PKG-INFO');
-      for (const [path, buffer] of Object.entries(zip)) {
-        const pyPath = path.replaceAll(`${name}-${packageInfo.version}/`, '');
-
-        const subPath = path.match('/.+/');
-        if (subPath) {
-          const libPath = join(PACKAGE_PATH, subPath[0]);
-          await createDirIfNotExist(libPath);
-        }
-
-        const file = join(PACKAGE_PATH, pyPath);
-        await writeFile(file, buffer);
-      }
+      await install(data);
     }
   });
 }
